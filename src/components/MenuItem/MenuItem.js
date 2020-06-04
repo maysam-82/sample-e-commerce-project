795514cfcfa431250d61879c7f4597c5ab5dd47d
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-06-04 20:36:29 +1000

    add google font, update style of MenuItem

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -2,17 +2,13 @@ import React from 'react';
 import classes from './menuItem.module.scss';
 
 const MenuItem = ({ title, imageUrl, imageSize }) => {
+	let menuItemClass = [classes.menuItem];
+	if (imageSize === 'large') {
+		menuItemClass.push(classes.menuItemLarge);
+	}
 	return (
-		<div className={classes.menuItem}>
-			<img
-				src={imageUrl}
-				className={
-					imageSize !== 'large'
-						? classes.menuItemImage
-						: classes.menuItemImageLarge
-				}
-				alt={title}
-			/>
+		<div className={menuItemClass.join(' ')}>
+			<img src={imageUrl} className={classes.menuItemImage} alt={title} />
 			<div className={classes.content}>
 				<h1 className={classes.title}>{title}</h1>
 				<span className={classes.subtitle}>SHOP NOW</span>
