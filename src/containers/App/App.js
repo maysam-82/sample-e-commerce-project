2e57c7172e0add44ee804fffbddb49586250da34
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-06-05 17:25:12 +1000

    refactor App to functional

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,29 +8,17 @@ import Header from '../../components/Header/Header';
 import SignInSignUp from '../../pages/SignInSignUp';
 import classes from './app.module.scss';
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			menuItems,
-			shopData,
-		};
-	}
-
-	render() {
-		const { menuItems, shopData } = this.state;
-		return (
-			<div className={classes.appContainer}>
-				<Header />
-				<Switch>
-					<Route exact path="/" render={() => <Home menuItems={menuItems} />} />
-					<Route path="/shop" render={() => <Shop shopData={shopData} />} />
-					<Route path="/signin" component={SignInSignUp} />
-				</Switch>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div className={classes.appContainer}>
+			<Header />
+			<Switch>
+				<Route exact path="/" render={() => <Home menuItems={menuItems} />} />
+				<Route path="/shop" render={() => <Shop shopData={shopData} />} />
+				<Route path="/signin" component={SignInSignUp} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
