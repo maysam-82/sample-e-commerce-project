c8b7a2056e9f1ffb961ad9701cbacea7e40ab377
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-06-05 22:57:27 +1000

    implement auth logic in App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../../pages/Home';
 import menuItems from '../../data/menuItems.data';
@@ -6,9 +6,30 @@ import shopData from '../../data/shop.data';
 import Shop from '../../pages/Shop/Shop';
 import Header from '../../components/Header/Header';
 import SignInSignUp from '../../pages/SignInSignUp';
+import { auth } from '../../firebase/firebase';
 import classes from './app.module.scss';
 
 function App() {
+	const [currentUser, setCurrentUser] = useState(null);
+	useEffect(() => {
+		// `onAuthStateChanged` observer will Get the currently signed-in user. This is an open messaging system or listener between firebase and application. This connection always opens
+		// while application is mounted.
+		// if (user) {
+		// User is signed in.
+		// } else {
+		// No user is signed in.
+		// }
+		// By the use of `onAuthStateChanged` we do not need to fetch
+		// manually everytime we are going to check if the status changed.
+		auth.onAuthStateChanged((user) => setCurrentUser(user));
+		// We need to close this connection whenever we do not want it.
+		// If useEffect returns a function, React will run it when it is time to clean up.
+		// Acts like componentWillUnmount lifecycle hook.
+		return function cleanup() {
+			auth.onAuthStateChanged((user) => setCurrentUser(user));
+		};
+	}, [currentUser]);
+
 	return (
 		<div className={classes.appContainer}>
 			<Header />
