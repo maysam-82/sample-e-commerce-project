097bd588902d9802133ae70c1dd92362922a80cb
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-06-10 11:48:28 +1000

    implement redux-persist for persist data

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './containers/App';
 import { BrowserRouter } from 'react-router-dom';
 import reducers from './reducers';
 import './index.css';
+
 const middlewares = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
 	reducers,
 	composeEnhancers(applyMiddleware(...middlewares))
 );
+const persistor = persistStore(store);
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.querySelector('#root')
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// import localStorage
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/userReducer';
 import cartReducer from './cart/cartReducer';
 
-export default combineReducers({
+const persistConfig = {
+	key: 'root',
+	storage,
+	// whitelist is an array that contains list of name (key)of all reducers which we are going to store
+	whitelist: ['cart'],
+};
+
+const reducers = combineReducers({
 	users: userReducer,
 	cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, reducers);
