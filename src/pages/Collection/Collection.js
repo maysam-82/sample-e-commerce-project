c892f2e45d187b99175ecce4e1ad20ff921b3ae5
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-06-10 21:56:08 +1000

    update Collection with styles

--- src/pages/Collection/Collection.js
+++ src/pages/Collection/Collection.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectShoppingCollection } from '../../reducers/shop/shopSelector';
+import CategoryItem from '../../containers/CategoryItem/CategoryItem';
 
 import classes from './collection.module.scss';
 
 function Category({ collection }) {
+	const { title, items } = collection;
+	function renderCollectionItems() {
+		return items.map((item) => <CategoryItem key={item.id} item={item} />);
+	}
 	return (
 		<div className={classes.collection}>
-			<h2>CATEGORY PAGE</h2>
+			<h2 className={classes.title}>{title}</h2>
+			<div className={classes.items}>{renderCollectionItems()}</div>
 		</div>
 	);
 }
